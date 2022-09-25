ff4a731a45b790259c46240e8556ee49234875b3
aniket1234321 <104294426+aniket1234321@users.noreply.github.com>
2022-09-25 21:34:38 +0530

    Add files via upload

---
+++ ANIKET KALIA.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int age;
+    string name;
+    node *next;
+    node *prev;
+    node(string s, int x)
+    {
+        name = s;
+        age = x;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void printtheLL(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->name << "-" << head->age << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    node *father = new node("SANJEEV KALIA", 45);
+    node *mother = new node("SUMAN LATA SHARMA", 43);
+    node *myself = new node("ANIKIT KALIA", 20);
+    node *sister = new node("ANAMIKA KALIA", 16);
+    node *brother = new node("ISHAAN KALIA", 14);
+
+    node *head = father;
+
+    father->prev = NULL;
+    father->next = mother;
+    mother->prev = father;
+    mother->next = myself;
+    myself->prev = mother;
+    myself->next = sister;
+    sister->prev = myself;
+    sister->next = brother;
+    brother->next = NULL;
+    brother->prev = sister;
+
+    printtheLL(head);
+
+    return 0;
+}
